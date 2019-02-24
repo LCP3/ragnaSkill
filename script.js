39c0ab7da4e1ec781adaac0553e7b61c1468d59a
LCP3 <lcp3.dev@gmail.com>
2019-02-24 09:45:28 -0600

    Added dropdown functionality for class selection

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 var lvl = 0;
 // Default max level
 var maxlvl = 40;
-// Init var to hold data
+// Starting global skill level
+var baseSkillLevel = 0;
+// Init var to hold JSON data
 var classdata;
 
 // Self-reminder for arrow function equivalent
@@ -15,25 +17,19 @@ var classdata;
 //   console.log(data);
 // });
 
+
+// Class Selection
+// Select dropdown
+var myList = document.querySelector('select');
+
+// Fetch the JSON
 fetch('./skills.json')
-.then(res => res.json())
-.then(data => classdata = data)
-.then(i => console.log(classdata));
-
-
-
-
-
-
-// console.log(data);
-// Iterate through classdata for individual classes
-
-// Display classes
-// classdata.class.forEach(i => {
-//   console.log(i); //Acolyte
-// });
-
-//Choose a class
-
-
-// Display skills for chosen class
+.then(res => res.json()) // Send JSON response
+.then(function(data) {
+    for (i=0;i<data.classes.length;i++) { // Loop through JSON and populate dropdown options
+        var option = document.createElement('option');
+        option.innerHTML = data.classes[i].name;
+        option.setAttribute('value', data.classes[i].name.toLowerCase());
+        myList.appendChild(option);
+    }
+});
