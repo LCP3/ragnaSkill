d4f6e7a9e23425620cbe7de2502aa78859029e7f
LCP3 <lcp3.dev@gmail.com>
2019-02-27 08:10:10 -0600

    Fully updated skills.json for base functionality

--- script.js
+++ script.js
@@ -21,15 +21,59 @@ var classdata;
 // Class Selection
 // Select dropdown
 var myList = document.querySelector('select');
+// Select skills container
+var skillsContainer = document.getElementById('skillsContainer');
 
 // Fetch the JSON
 fetch('./skills.json')
 .then(res => res.json()) // Send JSON response
 .then(function(data) {
     for (i=0;i<data.classes.length;i++) { // Loop through JSON and populate dropdown options
-        var option = document.createElement('option');
-        option.innerHTML = data.classes[i].name;
-        option.setAttribute('value', data.classes[i].name.toLowerCase());
-        myList.appendChild(option);
+        var classOpt = document.createElement('option');
+        classOpt.innerHTML = data.classes[i].name;
+        classOpt.setAttribute('value', data.classes[i].name.toLowerCase());
+        myList.appendChild(classOpt);
     }
+    jobChoice(data); //Callback to event listener
+    // showSkills(data);
 });
+
+
+
+// When a class is chosen
+function jobChoice(data) {
+    myList.addEventListener('change', function(jobClass) {
+        console.log(this.value);
+        console.log(this.selectedIndex);
+        // Clear the current skill list
+        skillsContainer.innerHTML = '';
+        // Add all skills to container
+        // for(i=this.selectedIndex;i<data.classes[i].skill.length;i++) {
+        //     var skill = document.createElement('p');
+        //     data.classes[i].skill.forEach(function(e) {
+        //         console.log(e);
+        //     });
+        //     // skill.innerHTML = data.classes[i].skill.name;
+        //     console.log(data.classes[i].skill.length);
+        //     skillsContainer.appendChild(skill);
+        // }
+        var i = this.selectedIndex;
+
+        data.classes[i].skill.forEach(function(item) {
+            var skill = document.createElement('p');
+            skill.innerHTML = item.name;
+            skillsContainer.appendChild(skill);
+        });
+        
+
+
+    });
+}
+
+// i = 0 -- classes[0].skill[0].name
+// selecting class 3
+// i=0
+// i needs to equal the index of the chosen class
+// acolyte = i = 0
+// priest = i = 1
+// monk = i = 2
