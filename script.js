db7b559c9efff26704936277b5ad9cdf28fd0d57
LCP3 <lcp3.dev@gmail.com>
2019-03-03 13:58:10 -0600

    Quick refactor of code building out the ranks for eacb skill in script.js using ES6 template literals

--- script.js
+++ script.js
@@ -47,33 +47,40 @@ function jobChoice(data) {
         console.log(this.selectedIndex);
         // Clear the current skill list
         skillsContainer.innerHTML = '';
-        // Add all skills to container
-        // for(i=this.selectedIndex;i<data.classes[i].skill.length;i++) {
-        //     var skill = document.createElement('p');
-        //     data.classes[i].skill.forEach(function(e) {
-        //         console.log(e);
-        //     });
-        //     // skill.innerHTML = data.classes[i].skill.name;
-        //     console.log(data.classes[i].skill.length);
-        //     skillsContainer.appendChild(skill);
-        // }
+        // Store index of skills for looping purposes
         var i = this.selectedIndex;
-
         data.classes[i].skill.forEach(function(item) {
-            var skill = document.createElement('p');
-            skill.innerHTML = item.name;
-            skillsContainer.appendChild(skill);
+            // Create HTML elements for skill info to be placed
+            var skillDiv = document.createElement('div');
+            skillDiv.setAttribute('class', 'skill');
+            skillsContainer.appendChild(skillDiv);
+            
+            // var skillName = document.createElement('p');
+            // skillName.innerHTML = item.name;
+            // skillDiv.appendChild(skillName);
+
+            // var skillRank = document.createElement('span');
+            // skillRank.setAttribute('class', 'skillRank');
+            // skillRank.innerHTML = baseSkillLevel;
+            // skillName.appendChild(skillRank);
+
+            // var skillDivider = document.createElement('p');
+            // skillDivider.innerHTML = " / ";
+            // skillDiv.appendChild(skillDivider);
+
+            // var skillMax = document.createElement('span');
+            // skillMax.innerHTML = item.maxRank;
+            // skillDiv.appendChild(skillMax);
+
+            // Refactor/Clean up w/ ES6 Template literal.  SICK.
+            // Store data to be built w/ template literal
+            var skillName = item.name;
+            var skillRank = baseSkillLevel;
+            var skillMax = item.maxRank;
+            // Build the html for each skill
+            skillDiv.innerHTML = `<p>${skillName} Rank: <span class='skillRank'>${skillRank}</span> of <span class='skillMax'>${skillMax}`;
         });
-        
-
-
     });
 }
 
-// i = 0 -- classes[0].skill[0].name
-// selecting class 3
-// i=0
-// i needs to equal the index of the chosen class
-// acolyte = i = 0
-// priest = i = 1
-// monk = i = 2
+{/* <p>Medatation Rank:<span>0</span><p> "/" </p><span>10</span></p> */}
