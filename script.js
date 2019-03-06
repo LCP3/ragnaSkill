a4a45591c4272be66a420151a707d526071942ce
LCP3 <lcp3.dev@gmail.com>
2019-03-06 16:14:26 -0600

    Cleaned up code, added click functionality for skills.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Starting player level
 var lvl = 0;
-// Default max level
+// Default max player level
 var maxlvl = 40;
 // Starting global skill level
 var baseSkillLevel = 0;
@@ -16,7 +16,6 @@ var classdata;
 // .then(function(data){
 //   console.log(data);
 // });
-
 
 // Class Selection
 // Select dropdown
@@ -34,53 +33,51 @@ fetch('./skills.json')
         classOpt.setAttribute('value', data.classes[i].name.toLowerCase());
         myList.appendChild(classOpt);
     }
-    jobChoice(data); //Callback to event listener
-    // showSkills(data);
+    jobChoice(data); //Callback to event listener for class choice
 });
 
 
 
 // When a class is chosen
 function jobChoice(data) {
-    myList.addEventListener('change', function(jobClass) {
-        console.log(this.value);
-        console.log(this.selectedIndex);
+    // Listen for a change of the class select dropdown
+    myList.addEventListener('change', function() {
         // Clear the current skill list
         skillsContainer.innerHTML = '';
-        // Store index of skills for looping purposes
-        var i = this.selectedIndex;
-        data.classes[i].skill.forEach(function(item) {
-            // Create HTML elements for skill info to be placed
+        // Store index of selected class's skills
+        var index = this.selectedIndex;
+        // For each skill of the selected class
+        data.classes[index].skill.forEach(function(item, i) {
+            // Create div for skill info to be placed into
             var skillDiv = document.createElement('div');
             skillDiv.setAttribute('class', 'skill');
             skillsContainer.appendChild(skillDiv);
-            
-            // var skillName = document.createElement('p');
-            // skillName.innerHTML = item.name;
-            // skillDiv.appendChild(skillName);
-
-            // var skillRank = document.createElement('span');
-            // skillRank.setAttribute('class', 'skillRank');
-            // skillRank.innerHTML = baseSkillLevel;
-            // skillName.appendChild(skillRank);
-
-            // var skillDivider = document.createElement('p');
-            // skillDivider.innerHTML = " / ";
-            // skillDiv.appendChild(skillDivider);
-
-            // var skillMax = document.createElement('span');
-            // skillMax.innerHTML = item.maxRank;
-            // skillDiv.appendChild(skillMax);
 
             // Refactor/Clean up w/ ES6 Template literal.  SICK.
-            // Store data to be built w/ template literal
+            // Store data to be built out w/ template literals
             var skillName = item.name;
             var skillRank = baseSkillLevel;
             var skillMax = item.maxRank;
-            // Build the html for each skill
+            // Build the html for the skill
             skillDiv.innerHTML = `<p>${skillName} Rank: <span class='skillRank'>${skillRank}</span> of <span class='skillMax'>${skillMax}`;
+
+            // Select newly created div and add click listener
+            addClickListener(i);
         });
     });
 }
 
-{/* <p>Medatation Rank:<span>0</span><p> "/" </p><span>10</span></p> */}
+function addClickListener(i) {
+    // Select current skill, current rank + the containing <span>, and the max rank of the skill
+    var skill = document.querySelectorAll(".skill")[i];
+    var skillRank = parseInt(document.querySelectorAll(".skillRank")[i].innerHTML);
+    var skillRankSpan = document.querySelectorAll(".skillRank")[i];
+    var skillMax = parseInt(document.querySelectorAll(".skillMax")[i].innerHTML);
+    // Click event for adding ranks clicked skill
+    skill.addEventListener('click', function() {
+        if (skillRank < skillMax) {
+            skillRank++;
+            skillRankSpan.innerHTML = skillRank;
+        }
+    });
+}
